55bfcd778efe712501a5052e50a0baaec2aaff1b
moinmansoori112 <moinmansoori112@gmail.com>
2022-02-27 00:55:53 +0530

    mytask

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh87698-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://Aafrin77:omaJBV2vPYhwOS7f@cluster0.ekfff.mongodb.net/moin9970", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
