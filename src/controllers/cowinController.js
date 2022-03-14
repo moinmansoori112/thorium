2e9c6d120cde51024e6049c7106c3afbb200b12a
moinmansoori112 <moinmansoori112@gmail.com>
2022-03-14 11:37:22 +0530

    Axios-Promises-Task

--- src/controllers/cowinController.js
+++ src/controllers/cowinController.js
@@ -1,4 +1,5 @@
-let axios = require("axios")
+let axios = require("axios");
+const { response } = require("express");
 
 
 let getStates = async function (req, res) {
@@ -44,8 +45,8 @@ let getByPin = async function (req, res) {
         let date = req.query.date
         console.log(`query params are: ${pin} ${date}`)
         var options = {
-            method: "get",
-            url: `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=${pin}&date=${date}`
+         method: "get",
+         url: `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=${pin}&date=${date}`
         }
         let result = await axios(options)
         console.log(result.data)
@@ -56,6 +57,7 @@ let getByPin = async function (req, res) {
         res.status(500).send({ msg: err.message })
     }
 }
+
 
 let getOtp = async function (req, res) {
     try {
@@ -77,9 +79,93 @@ let getOtp = async function (req, res) {
         res.status(500).send({ msg: err.message })
     }
 }
+let getCenter = async function (req, res) {
+    try {
+        let pin = req.query.district_id
+        let date = req.query.date
+        console.log(`query params are: ${pin} ${date}`)
+        var options = {
+            method: "get",
+            url: `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=${pin}&date=${date}`
+        }
+        let result = await axios(options)
+        console.log(result.data)
+        res.status(200).send({ msg: result.data })
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).send({ msg: err.message })
+    }
+}
 
 
+let getWeather = async function (req, res) {
+    try {
+        let q = req.query.q
+        let appid = req.query.appid
+        console.log(`query params are: ${q} ${appid}`)
+        var options = {
+            method: "get",
+            url: `http://api.openweathermap.org/data/2.5/weather?q=${q}&appid=${appid}`
+        }
+        let result = await axios(options)
+        console.log(result.data)
+        res.status(200).send({ msg: result.data })
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).send({ msg: err.message })
+    }
+}
+
+
+let getSortedCities = async function (req, res) {
+    try {
+        let cities = ["Bengaluru","Mumbai", "Delhi", "Kolkata", "Chennai", "London", "Moscow"]
+        let cityObjArr = []
+
+        for (i=0; i<cities.length; i++){
+            
+            let obj= { city : cities[i] }
+            resp= await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${cities[i]}&appid=746cc0753017e60a0327f4dd8a99d10a`)
+            console.log(resp.data.main.temp)
+
+            obj.temp= resp.data.main.temp
+            cityObjArr.push(obj)
+        }
+        
+        let sorted = cityObjArr.sort( function (a,b) {return a.temp - b.temp})
+        console.log (sorted) 
+        res.status(200).send({status : true, data:sorted})
+
+    }
+    catch (error) {
+        console.log(error)
+        res.status(500).send({ status: false,msg: "server error"})
+    }
+}
+
+let memeHandler = async function (req,res){
+
+    try {
+         let options = {
+             mehtod : "post",
+             url : 'https://api.imgflip.com/caption_image?template_id=87743020&text0=K.G.F&text1=Laal singh Chaddha&username=chewie12345&password=meme@123'
+       }
+       let result= await axios(options)
+       res.send({ data: result.data})
+    }
+     catch (error) {
+         console.log(error)
+         res.status(500).send({ status: false , msg: "server error"})
+     }
+
+}
 module.exports.getStates = getStates
 module.exports.getDistricts = getDistricts
 module.exports.getByPin = getByPin
 module.exports.getOtp = getOtp
+module.exports.getCenter= getCenter
+module.exports.getWeather= getWeather
+module.exports.getSortedCities = getSortedCities
+module.exports.memeHandler = memeHandler
